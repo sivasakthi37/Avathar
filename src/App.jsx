23a630ebea42b675147f63b122d402b3878a2e90
qburst-sivasakthi <sivasakthi@qburst.com>
2024-04-09 19:40:38 +0530

    postMessgae added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
-import { Loader } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Leva } from "leva";
-import { Experience } from "./components/Experience";
-import { UI } from "./components/UI";
-
+import { Loader } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Leva } from 'leva'
+import { Experience } from './components/Experience'
+import { UI } from './components/UI'
+import { useEffect } from 'react'
+import { useChat } from './hooks/useChat'
 function App() {
+  const { chat } = useChat()
+  useEffect(() => {
+    console.log('UseEffect trigger')
+    const handleMessage = (event) => {
+      console.log('event', event)
+      const message = JSON.parse(event.data)
+      console.log('Received message:', message)
+
+      if (message.action === 'playAudio') {
+        console.log('action event trigger')
+        chat()
+      }
+    }
+
+    window.addEventListener('message', handleMessage)
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('message', handleMessage)
+    }
+  }, [])
+
   return (
     <>
-      <Loader />
-      <Leva hidden />
-      <UI />
-      <Canvas shadows camera={{ position: [0, 0, 1], fov: 30 }}>
-        <Experience />
-      </Canvas>
+      <div style={{ width: '100%', height: '100vh', zIndex: 100 }}>
+        <Loader />
+        <Leva hidden />
+        <UI hidden={true} />
+        <Canvas shadows camera={{ position: [0, 0, 1], fov: 30 }}>
+          <Experience />
+        </Canvas>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
